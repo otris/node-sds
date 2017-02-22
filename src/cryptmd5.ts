e19b509dd6087c9bf8e48bd0881f3d47d1a7bc6c
Benjamin Kircher <benjamin.kircher@gmail.com>
2017-02-22 16:00:50 +0100

    Add copyright header to cryptmd5.ts

--- src/cryptmd5.ts
+++ src/cryptmd5.ts
@@ -1,13 +1,41 @@
 /**
- * Implementation based on source from freebsd module crypt-md5.c from Poul-Henning Kamp.
- * Found on https://github.com/freebsd/freebsd/blob/master/lib/libcrypt/crypt-md5.c 
- * 
- * Other sources are found on 
- * https://github.com/joyent/syslinux/blob/master/com32/libutil/crypt-md5.c 
- * and
- * https://pythonhosted.org/passlib/lib/passlib.hash.md5_crypt.html
- * 
- * latest recall of these webpages : 22.02.2017 
+ * Implementation based on libcrypt from Poul-Henning Kamp which can be found here:
+ *
+ *     https://github.com/freebsd/freebsd/blob/master/lib/libcrypt/crypt-md5.c
+ *
+ * and therefore governed by its license.
+ *
+ * Copyright (c) 2003 Poul-Henning Kamp
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Other sources that helped during development can be found here:
+ *
+ *     https://github.com/joyent/syslinux/blob/master/com32/libutil/crypt-md5.c
+ *
+ * and here:
+ *
+ *     https://pythonhosted.org/passlib/lib/passlib.hash.md5_crypt.html
  */
 
 'use strict';
