b469a8ccc2361fc410540e62fc94da3e323dd845
Maximilian Wehrstedt <wehrstedt@otris.de>
2018-07-19 16:04:54 +0200

    class for creating sds responses

--- src/sds/SDSMessage.ts
+++ src/sds/SDSMessage.ts
@@ -2,7 +2,61 @@
  * Names of parameters and return values of server side operations
  */
 export enum ParameterNames {
+	CLIENT_ID = 1,
+	CLASS_NAME = 2,
+	VALUE = 4,
+	RETURN_VALUE = 5,
+	SOMETHING = 8,
+	INDEX = 13,
+	LANGUAGE = 14,
+	CLASS_ID = 16,
+	IS_TRANSACTION_OBJECT = 18,
+	USER = 21,
+	PASSWORD = 22,
+	LAST = 25,
+	PROPERTIES = 29,
+	USER_ID = 40,
+	PARAMETER = 48,
+	PARAMETER_PDO = 49,
+	CONVERSION = 51,
+	INIT = 53,
+	PRINCIPAL = 80,
+	FILENAME = 87,
+	OP_CODE = 88,
+	FLAG = 119,
 }
+
+/**
+ * Maps the paramter names to a specific type. This list has to be maintenend
+ * parallel to the enum "ParameterNames"
+ * Note: This list is incomplete. It's possible, that a parameter name has more types than defined here
+ */
+/* tslint:disable:member-ordering */
+export interface IParameterNamesTypesMap {
+	1: number;
+	2: string;
+	4: number | string;
+	5: string | string[] | boolean | number;
+	8: number;
+	13: number;
+	14: number;
+	16: number;
+	18: boolean;
+	21: string;
+	22: string;
+	25: number;
+	29: boolean;
+	40: number;
+	48: string | string[];
+	49: string[];
+	51: boolean;
+	53: boolean;
+	80: string;
+	87: string;
+	88: number;
+	119: any; // TODO: Missing type informations
+}
+/* tslint:enable:member-ordering */
 
 export enum Operations {
 }
@@ -10,8 +64,12 @@ export enum Operations {
 export enum Types {
 	BOOLEAN = 2,
 	INT32 = 3,
+	DATE = 4,
 	STRING = 7,
+	OID = 9,
+	INT32_LIST = 10,
 	STRING_LIST = 11,
+	OID_LIST = 12,
 	NULL_FLAG = 128,
 }
 
@@ -29,8 +87,25 @@ export abstract class SDSMessage {
 	/** Initial size of the created buffer */
 	private INITIAL_BUFFER_SIZE = 4096;
 
-	constructor() {
-		this.buffer = Buffer.alloc(this.INITIAL_BUFFER_SIZE);
-		this.bufferedLength = 0;
+	constructor(buffer?: Buffer) {
+		if (buffer) {
+			if (buffer.length < 12) {
+				throw new Error(`The buffer has to be at least 12 bytes long, got ${buffer.length}`);
+			}
+
+			this.buffer = buffer;
+			this.bufferedLength = buffer.length;
+		} else {
+			this.buffer = Buffer.alloc(this.INITIAL_BUFFER_SIZE);
+			this.bufferedLength = 0;
+		}
+	}
+
+	/**
+	 * Determines if the request/response is a "simple message"
+	 * @returns true if it's a simple message, otherwise false
+	 */
+	protected isSimple(): boolean {
+		return this.buffer.length === 8;
 	}
 }
---
+++ src/sds/SDSResponse.ts
@@ -0,0 +1,273 @@
+import * as assert from "assert";
+import { ntohl } from "../network";
+import { IParameterNamesTypesMap, Operations, ParameterNames, SDSMessage, Types } from "./SDSMessage";
+
+/** The first index a parameter can occur in a valid sds message */
+const FIRST_PARAM_INDEX = 13;
+
+/** Map with the paramter names (key = value of the enum, value = Name of the enum property) */
+const parameterNamesMap: Map<ParameterNames, string> = (() => {
+	const parameterNamesKeys = Object.keys(ParameterNames);
+	const _parameterNamesMap = new Map();
+	for (let i = parameterNamesKeys.length / 2; i < parameterNamesKeys.length; i++) {
+		_parameterNamesMap.set(parseInt(parameterNamesKeys[i - parameterNamesKeys.length / 2], 10), parameterNamesKeys[i]);
+	}
+
+	return _parameterNamesMap;
+})();
+
+/** Map with the paramter names (key = value of the enum, value = Name of the enum property) */
+const typesMap: Map<Types, string> = (() => {
+	const typesKeys = Object.keys(Types);
+	const _typesMap = new Map();
+	for (let i = typesKeys.length / 2; i < typesKeys.length; i++) {
+		_typesMap.set(parseInt(typesKeys[i - typesKeys.length / 2], 10), typesKeys[i]);
+	}
+
+	return _typesMap;
+})();
+
+interface IResponseParameter {
+	name: string;
+	nameIndex: ParameterNames;
+	type: string;
+	typeIndex: Types;
+	value: any;
+}
+
+export class SDSResponse extends SDSMessage {
+	/** Holds the object id to operate on. By default, it's empty */
+	private _oId: string;
+
+	/** Holds the operation to execute on the server side */
+	private _operation: Operations;
+
+	/** Map with all response parameters of the response */
+	private parameters: Map<ParameterNames, IResponseParameter>;
+
+	/**
+	 * Returns the object id the response belongs to
+	 * @returns The object id the response belongs to
+	 */
+	public get oId(): string {
+		if (this._oId.length < 1) {
+			// Byte 1 - 4 = Länge der Nachricht
+			// Byte 5 - 12 = OID
+			const oIdFirst = ntohl(this.buffer, 4);
+			const oIdLast = ntohl(this.buffer, 8);
+			this._oId = `${oIdFirst}:${oIdLast}`;
+		}
+
+		return this._oId;
+	}
+
+	/**
+	 * Returns the operation which should be executed
+	 * @returns Operation which should be executed
+	 */
+	public get operation(): Operations {
+		if (this._operation < 0) {
+			this._operation = this.buffer[12];
+		}
+
+		return this._operation;
+	}
+
+	constructor(buffer: Buffer) {
+		super(buffer);
+		this._oId = "";
+		this._operation = -1;
+
+		// create a map of parameters. It's easier for debugging and allows us to return requested paramters by
+		// using the map
+		this.parameters = this.parseResponseParameters();
+	}
+
+	/**
+	 * Extracts the passed parameter from the response
+	 * @param parameterName Name of the parameter
+	 * @returns The value of the passed parameter
+	 */
+	public getParameter<T extends ParameterNames>(parameterName: T): IParameterNamesTypesMap[T] {
+		const responseParameter = this.parameters.get(parameterName);
+		if (responseParameter) {
+			return responseParameter.value;
+		} else {
+			throw new Error(`Unknown paramter ${parameterName}`);
+		}
+	}
+
+	/**
+	 * Returns the value of a parameter
+	 * @param paramIndex Start index of the parameter
+	 * @param headType Suggested parameter type (needed for validation)
+	 * @returns The value of the parameter
+	 */
+	private getBooleanParameter(paramIndex: number, headType: Types): boolean {
+		assert.ok((headType & ~Types.NULL_FLAG) === Types.BOOLEAN);
+		if (headType & Types.NULL_FLAG) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * Returns the value of a parameter
+	 * @param paramIndex Start index of the parameter
+	 * @param headType Suggested parameter type (needed for validation)
+	 * @returns The value of the parameter
+	 */
+	private getIntParameter(paramIndex: number, headType: Types): number {
+		assert.ok((headType & ~Types.NULL_FLAG) === Types.INT32);
+		return ntohl(this.buffer, paramIndex + 2);
+	}
+
+	/**
+	 * Returns the value of a parameter
+	 * @param paramIndex Start index of the parameter
+	 * @param headType Suggested parameter type (needed for validation)
+	 * @returns The value of the parameter
+	 */
+	private getStringListParameter(paramIndex: number, headType: Types): string[] {
+		assert.ok((headType & ~Types.NULL_FLAG) === Types.STRING_LIST);
+		if (headType & Types.NULL_FLAG) {
+			return [];
+		}
+
+		// ----- header of parameter -----
+		// paramIndex[0]: type
+		// paramIndex[1]: name-code
+		// ----- data-part of parameter -----
+		// paramIndex[2..5]: size of data-part of the parameter (StringList)
+		// paramIndex[6..9]: size of the StringList (number of elements)
+		// paramIndex[10..13]: strLen: size of the first string (bytes)
+		// paramIndex[14...]: first string
+		// paramIndex[14 + strLen ...]: size of second string
+		// paramIndex[14 + strLen + 4 ...]: second string
+		// ...
+
+		// const dataPartSize = ntohl(this.buffer, paramIndex + 2);
+
+		const numElem = ntohl(this.buffer, paramIndex + 6);
+		const returnList: string[] = [];
+		let listPtr = paramIndex + 10;
+
+		for (let i = 0; i < numElem; i++) {
+			const strLen = ntohl(this.buffer, listPtr);
+			listPtr += 4;
+			const str = this.buffer.toString("utf8", listPtr, listPtr + strLen - 1);
+			listPtr += strLen;
+			returnList.push(str);
+		}
+
+		return returnList;
+	}
+
+	/**
+	 * Returns the value of a parameter
+	 * @param paramIndex Start index of the parameter
+	 * @param headType Suggested parameter type (needed for validation)
+	 * @returns The value of the parameter
+	 */
+	private getStringParameter(paramIndex: number, headType: Types): string {
+		assert.ok((headType & ~Types.NULL_FLAG) === Types.STRING);
+		if (headType & Types.NULL_FLAG) {
+			return "";
+		}
+
+		const strLength = ntohl(this.buffer, paramIndex + 2) - 1;
+		// Note: we expect here that the opposite party is a JANUS server compiled with UTF-8 support.
+		return this.buffer.toString("utf8", paramIndex + 6, paramIndex + 6 + strLength);
+	}
+
+	/**
+	 * Determines the size of the parameter
+	 * @param paramIndex Index of the parameter in the message buffer
+	 * @returns Size of the parameter
+	 */
+	private paramLength(paramIndex: number): number {
+
+		// head: 2 bytes
+		//       head.type: 1 byte
+		//       head.name: 1 byte
+		//
+		// data: 0 or more bytes, depending on head.type
+		//       if head.type is Type.Int32 or Type.Date: 4 bytes
+		//       if head.type is Type.OID: 8 bytes
+		//       and so on
+
+		assert.ok(paramIndex >= FIRST_PARAM_INDEX && paramIndex < this.buffer.length);
+
+		const headType = this.buffer[paramIndex];
+
+		if (headType & Types.NULL_FLAG) {
+			// No data, just the head
+			return 2;
+		}
+
+		switch (headType & ~Types.NULL_FLAG) {
+			case Types.BOOLEAN:
+				return 2;
+			case Types.INT32:
+			case Types.DATE:
+				return 2 + 4;
+			case Types.OID:
+				// head: 2 + oid.low: 4 + oid.high: 4
+				return 2 + (2 * 4);
+			default:
+				// head: 2 + size: 4 + whatever the size is
+				return 2 + 4 + ntohl(this.buffer, paramIndex + 2);
+		}
+	}
+
+	/**
+	 * Creates a map with all return parameters of the response
+	 * @returns Map with all return parameters of the response
+	 */
+	private parseResponseParameters(): Map<ParameterNames, IResponseParameter> {
+		const responseParameters: Map<ParameterNames, IResponseParameter> = new Map();
+
+		for (let i = FIRST_PARAM_INDEX; i < this.bufferedLength; i += this.paramLength(i)) {
+			const responseParameter: IResponseParameter = {
+				name: parameterNamesMap.get(this.buffer[i + 1]) || "<unknown>",
+				nameIndex: this.buffer[i + 1],
+				type: typesMap.get(this.buffer[i] & ~Types.NULL_FLAG) || "<unknown>",
+				typeIndex: this.buffer[i] & ~Types.NULL_FLAG,
+				value: null,
+			};
+
+			switch (responseParameter.typeIndex) {
+				case Types.BOOLEAN:
+					responseParameter.value = this.getBooleanParameter(i, this.buffer[i]);
+					break;
+
+				case Types.INT32:
+					responseParameter.value = this.getIntParameter(i, Types.INT32);
+					break;
+
+				case Types.STRING:
+					const stringSize = ntohl(this.buffer, i + 2);
+					responseParameter.value = this.getStringParameter(i, Types.STRING);
+					break;
+
+				case Types.STRING_LIST:
+					const stringListSize = ntohl(this.buffer, i + 2);
+					responseParameter.value = this.getStringListParameter(i, Types.STRING_LIST);
+					break;
+
+				default:
+					throw new Error(`Unknown parameter type: ${responseParameter.typeIndex}`);
+			}
+
+			if (responseParameters.has(responseParameter.nameIndex)) {
+				// Should never happen, otherwise the JANUS-server is confused
+				throw new Error(`Parameter ${responseParameter.name} already exists in the parameters map. Invalid response.`);
+			} else {
+				responseParameters.set(responseParameter.nameIndex, responseParameter);
+			}
+		}
+
+		return responseParameters;
+	}
+}
