94577c17bedd5bf0cb32e1dc8998b2ad02b8fa0e
Maximilian Wehrstedt <wehrstedt@otris.de>
2018-08-23 11:55:59 +0200

    added missing exports to use the module

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
 export { SDSConnection } from "./sds/SDSConnection";
+export { SDSSimpleMessage } from "./sds/SDSSimpleMessage";
+export { SDSResponse } from "./sds/SDSResponse";
+export { SDSRequest } from "./sds/SDSRequest";
